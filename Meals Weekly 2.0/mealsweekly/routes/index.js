07fd34d354fa1fe8d5cfdaced92f46488081dca6
Jonah Kavadlo <jonahkavadlo@jonahs-mbp.attlocal.net>
2020-08-08 01:42:25 -0500

    Major updates to data handling

--- Meals Weekly 2.0/mealsweekly/routes/index.js
+++ Meals Weekly 2.0/mealsweekly/routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var globalUser = "";
+var globalWeek = "";
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -57,9 +59,21 @@ router.post('/adduser', function(req, res) {
 
 });
 
+
+
+
+
+
 /* GET Home page. */
 router.get('/home', function(req, res) {
-  res.render('home', { title: 'Meals Weekly' });
+  var db = req.db;
+  var mealcollection = db.get('mealcollection');
+  mealcollection.find({"username" : globalUser, "week" : globalWeek},{},function(e,docs){
+      res.render('home', {
+          title: 'Meals Weekly',
+          "meallist" : docs
+      });
+  });
 });
 
 /* GET List page. */
@@ -79,6 +93,8 @@ router.post('/sendmeals', function(req, res) {
   var db = req.db;
 
   // Get our form values. These rely on the "name" attributes
+  var week = req.body.week;
+  var day = req.body.day;
   var breakfast = req.body.breakfast;
   var lunch = req.body.lunch;
   var dinner = req.body.dinner;
@@ -87,11 +103,13 @@ router.post('/sendmeals', function(req, res) {
   var collection = db.get('mealcollection');
 
   // Submit to the DB
-  collection.insert({
+  collection.update(
+    {"day" : day, "week" : week, "username" : globalUser},
+    {$set:{
       "breakfast" : breakfast,
       "lunch" : lunch,
-      "dinner" : dinner
-  }, function (err, doc) {
+      "dinner" : dinner,
+  }}, function (err, doc) {
       if (err) {
           // If it failed, return error
           res.send("There was a problem adding the information to the database.");
@@ -103,5 +121,203 @@ router.post('/sendmeals', function(req, res) {
   });
 
 });
+
+
+
+
+
+/* GET Add Login page. */
+router.get('/login', function(req, res) {
+  res.render('login', { title: 'Meals Weekly' });
+});
+
+/* POST to Add Account service */
+router.post('/sendaccountdetails', function(req, res) {
+
+  // Set our internal DB variable
+  var db = req.db;
+
+  // Get our form values. These rely on the "name" attributes
+  var username = req.body.username;
+  var password = req.body.password;
+  globalUser = username;
+  globalWeek = "Week 1";
+
+  var usercollection = db.get('usercollection');
+  var mealcollection = db.get('mealcollection');
+
+  // Submit to the DB
+  usercollection.insert({
+      "username" : username,
+      "password" : password
+  }, function (err, doc) {
+      if (err) {
+          // If it failed, return error
+          res.send("There was a problem adding the information to the database.");
+      }
+      else {
+          // And forward to success page
+          mealcollection.insert([
+            {
+              "breakfast" : "",
+              "lunch" : "",
+              "dinner" : "",
+              "day" : "Monday",
+              "week" : globalWeek,
+              "username" : username
+            },
+            {
+              "breakfast" : "",
+              "lunch" : "",
+              "dinner" : "",
+              "day" : "Tuesday",
+              "week" : globalWeek,
+              "username" : username
+            },
+            {
+              "breakfast" : "",
+              "lunch" : "",
+              "dinner" : "",
+              "day" : "Wednesday",
+              "week" : globalWeek,
+              "username" : username
+            },
+            {
+              "breakfast" : "",
+              "lunch" : "",
+              "dinner" : "",
+              "day" : "Thursday",
+              "week" : globalWeek,
+              "username" : username
+            },
+            {
+              "breakfast" : "",
+              "lunch" : "",
+              "dinner" : "",
+              "day" : "Friday",
+              "week" : globalWeek,
+              "username" : username
+            },
+            {
+              "breakfast" : "",
+              "lunch" : "",
+              "dinner" : "",
+              "day" : "Saturday",
+              "week" : globalWeek,
+              "username" : username
+            },
+            {
+              "breakfast" : "",
+              "lunch" : "",
+              "dinner" : "",
+              "day" : "Sunday",
+              "week" : globalWeek,
+              "username" : username
+            },
+          ], function (err, doc) {
+            if (err) {
+                // If it failed, return error
+                res.send("There was a problem adding the information to the database.");
+            }
+            else {
+                // And forward to success page
+                res.redirect("home");
+            }
+        });
+      }
+  });
+
+});
+
+
+
+router.post('/newweek', function(req, res) {
+
+  // Set our internal DB variable
+  var db = req.db;
+
+  var newweek = req.body.newweekfield;
+  globalWeek = newweek;
+
+  // Set our collection
+  var collection = db.get('mealcollection');
+
+  // Submit to the DB
+  collection.insert([
+    {
+      "breakfast" : "",
+      "lunch" : "",
+      "dinner" : "",
+      "day" : "Monday",
+      "week" : newweek,
+      "username" : globalUser
+    },
+    {
+      "breakfast" : "",
+      "lunch" : "",
+      "dinner" : "",
+      "day" : "Tuesday",
+      "week" : newweek,
+      "username" : globalUser
+    },
+    {
+      "breakfast" : "",
+      "lunch" : "",
+      "dinner" : "",
+      "day" : "Wednesday",
+      "week" : newweek,
+      "username" : globalUser
+    },
+    {
+      "breakfast" : "",
+      "lunch" : "",
+      "dinner" : "",
+      "day" : "Thursday",
+      "week" : newweek,
+      "username" : globalUser
+    },
+    {
+      "breakfast" : "",
+      "lunch" : "",
+      "dinner" : "",
+      "day" : "Friday",
+      "week" : newweek,
+      "username" : globalUser
+    },
+    {
+      "breakfast" : "",
+      "lunch" : "",
+      "dinner" : "",
+      "day" : "Saturday",
+      "week" : newweek,
+      "username" : globalUser
+    },
+    {
+      "breakfast" : "",
+      "lunch" : "",
+      "dinner" : "",
+      "day" : "Sunday",
+      "week" : newweek,
+      "username" : globalUser
+    },
+  ], function (err, doc) {
+      if (err) {
+          // If it failed, return error
+          res.send("There was a problem adding the information to the database.");
+      }
+      else {
+          // And forward to success page
+          res.redirect("home");
+      }
+  });
+
+});
+
+router.post('/switchweek', function(req, res) {
+  var setweek = req.body.week;
+  globalWeek = setweek;
+  res.redirect("home");
+});
+
 
 module.exports = router;
